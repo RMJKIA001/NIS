7bedd332355f12ea4b23ce189c4c1c015b374398
PROBIE\Kiara <rmjkia001>
2019-05-12 22:16:24 +0200

    Updated ClientServer with simplified shared key

--- Client.java
+++ Client.java
@@ -1,192 +1,69 @@
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.DataOutputStream;
+import java.security.Security;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import java.security.SecureRandom;
+import java.util.Base64;
 
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
-public class Client {
+public class Client
+{
+	public static void main(String[] args) throws Exception
+	{
+		Security.addProvider(new BouncyCastleProvider());
+		Socket s = new Socket("127.0.0.1",3000);
+		ObjectInputStream dis = new ObjectInputStream(s.getInputStream());
+		ObjectOutputStream dos = new ObjectOutputStream(s.getOutputStream());
+		System.out.println(dis.readObject()); //reads welcome message
+		KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(128);
+        // Generate Key
+        SecretKey key = keyGenerator.generateKey();
+        dos.writeObject(key);
+       	// Generating IV.
+        byte[] IV = new byte[16];
+        SecureRandom random = new SecureRandom();
+        random.nextBytes(IV);
+        dos.writeObject(IV);
+        
+		DataOutputStream out =  new DataOutputStream(s.getOutputStream());
 
-    // I/O stuff
-    private ObjectInputStream sInput;
-    private ObjectOutputStream sOutput;
-    private Socket socket;
-    private String server, username;
-    private int port;
-    private ServerListener sl;
-    // Constructor for client
-    public Client(String server, int port, String username)
+		System.out.print("Enter message to send: ");
+		Scanner sc = new Scanner(System.in);
+		String msg = sc.nextLine();
+		byte[] encMsg = encrypt(msg.getBytes(), key,IV);
+		dos.writeObject(encMsg);
+		//out.writeUTF(Base64.getEncoder().encodeToString(encMsg));
+		System.out.println("Encrypted Text : "+Base64.getEncoder().encodeToString(encMsg) );
+		System.out.println("Message sent. Goodbye");
+	}
+
+	public static byte[] encrypt (byte[] plaintext,SecretKey key,byte[] IV ) throws Exception
     {
-        this.server = server;  
-        this.port = port;
-        this.username = username;
-    }
-
-    // Starts the client and dialog with server
-    public boolean start(){
-        try 
-        {
-            socket = new Socket(server, port);
-        } catch (IOException e) {
-            display("Error connecting to server: ");
-            return false;
-        }
-        String msg = "Connection accepted " + socket.getInetAddress() + ": " + socket.getPort();
-        display(msg);
-        msg = "- Use command 'LOGOUT' to logout of chat server. ";
-        display(msg);
-        // Create data streams
-        try {
-            sInput = new ObjectInputStream(socket.getInputStream());
-            sOutput = new ObjectOutputStream((socket.getOutputStream()));
-        } catch (IOException e) {
-            display("Exception in creating new streams");
-            return false;
-        }
-        try {
-            String usernames = (String) sInput.readObject();
-            System.out.println("Usernames being used at the moment are: " + usernames);
-            Scanner userscan = new Scanner(System.in);
-            while (usernames.contains(username))
-            {
-                System.out.println("Could you please choose a new username. This one has been taken.");
-                System.out.print("New username: ");
-                username = userscan.nextLine();
-            }
-            System.out.println("Username has been accepted");
-        } catch (IOException | ClassNotFoundException e) {
-        }
-
-        // Create thread to listen from server
-        sl = new ServerListener();
-        sl.start();
-
-        // Send username to server as String
-        try 
-        {
-            sOutput.writeObject(username);
-            System.out.println("Welcome to the server");
-        } catch (IOException e) {
-            display("Exception doing login");
-            disconnect();
-            return false;
-        }
-
-        // Success that client connected to server
-        return true;
-    }
-
-    public void display(String msg){
-        System.out.println(msg);
-    }
-    public void endSL()
-    {
-    //Ends the server Listener thread (Which caused the infinite loop)
-    sl.setT(false);
-    }
-    public void disconnect()
-    {    
-        try 
-        {
-            if(sInput != null) sInput.close();
-            if(sOutput != null) sOutput.close();
-            if(socket != null) socket.close();
-        }
-        catch(IOException e) {} 
-        System.exit(0);      //a bit of a cheat but this stops any errors from showing
-    }
-    //Send Message to server
-    public void sendMessage(Message msg)
-    {
-        try 
-        {
-            sOutput.writeObject(msg);
-        } catch (IOException e) {
-            display("Exception in sending message to the server");
-        }
-    }
-    public static void main (String[] args){
-        //default values
-        int portNumber = 1500;
-        String serverAddress = "localhost";
-        String userName = "HeWhoMustNotBeNamed";
-
-        // get values from command line
-        switch(args.length){
-            case 3:
-                serverAddress = args[2];
-            case 2:
-                portNumber = Integer.parseInt(args[1]);
-            case 1:
-                userName = args[0];
-            case 0:
-                break;
-            default:
-                System.out.println("Use command: java Client [username] [portNumber] " +
-                        "[serverAddress]");
-                return;
-        }
-
-        // create the Client object
-        Client client = new Client(serverAddress,portNumber,userName);
-       // System.out.println("DEBUG: Client created");
-        // try to connect to server
-        if (!client.start()){
-           // System.out.println("DEBUG: Client did not start");
-            return;
-        }
-
-        // get messages from the client
-        Scanner scanner = new Scanner(System.in);
-       // System.out.println("DEBUG: in scanner");
-        //infinite loop getting messages
-        while (true){
-            System.out.print(">> ");
-            String msg = scanner.nextLine();
-            // sends message to server that client wants to logout
-            if (msg.equalsIgnoreCase("LOGOUT"))
-            {
-                client.sendMessage(new Message(Message.LOGOUT, ""));
-                client.endSL();
-                break;
-            } 
-            else
-            {
-                client.sendMessage(new Message(Message.MESSAGE, msg));
-            }
-        }
-        client.disconnect();
-    }
-
-
-
-    // Inner class that listens to new messages from the server
-    class ServerListener extends Thread{
-        boolean t = true;
-        @Override
-        public void run(){
-            while (t){
-                try {
-                    String msg = (String) sInput.readObject();
-                    // TODO: add later
-                    if (msg.equalsIgnoreCase("DISCONNECT")){
-                        display("User disconnected");
-                        disconnect();
-                    } else {
-                        System.out.println(msg);
-                        System.out.print("> ");
-                    }
-                } catch (IOException e) {
-                    display("Connection to server is closed");
-                    t=false;
-                } catch (ClassNotFoundException e) {
-                    //display("I am the issue");
-
-                }
-            }
-        }
-        public void setT(boolean n){
-        t=n;}
+        //Get Cipher Instance
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        
+        //Create SecretKeySpec
+        SecretKeySpec keySpec = new SecretKeySpec(key.getEncoded(), "AES");
+        
+        //Create IvParameterSpec
+        IvParameterSpec ivSpec = new IvParameterSpec(IV);
+        
+        //Initialize Cipher for ENCRYPT_MODE
+        cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec);
+        
+        //Perform Encryption
+        byte[] cipherText = cipher.doFinal(plaintext);
+        
+        return cipherText;
     }
 }
-
--- Server.java
+++ Server.java
@@ -1,262 +1,73 @@
-import java.awt.*;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
+import java.io.DataInputStream;
+//import java.io.DataOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.ObjectInputStream;
+import java.security.Security;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import java.security.SecureRandom;
+import java.util.Base64;
 
-/**
- * 
- *
- * This class manages the connections and client threads
- */
-public class Server{
-    // Unique ID for each connection
-    private static int uniqueID;
-    // List of connected clients
-    private ArrayList<ClientThread> clients;
-    
-    private SimpleDateFormat sdf;
-    // port number that we listen for connection on
-    private int port;
-    // decides whether server will still run or not
-    private boolean on;
-    ServerSocket serverSocket;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
-    // Constructor for the server
-    // Can edit this to include GUI if needed
-    public Server (int port) throws IOException{
-        this.port = port;
-        sdf = new SimpleDateFormat("HH:mm:ss");
-        clients = new ArrayList<>();
-    }
-    
-    public void start(){
-        on = true;
-        try {
-            // Creates socket used by the server
-            serverSocket = new ServerSocket(port);
+public class Server
+{
+	
+	public static void main(String[] args) throws Exception
+	{
 
-            //infinite loop to check for connections
-            while(on){
-                 // Check if i should stop
-                if (!on){ break; }    
-                
-                // Show that server is waiting
-                display("Server is waiting for clients on port " + port + ".");
-               
-                // Accept connections
-                Socket socket = serverSocket.accept();
-               
-                // make a thread of the connection
-                ClientThread t = new ClientThread(socket); 
-                // save the thread in the client list
-                clients.add(t);
-                t.start();
-            }
-            
-            // possibly add try catches here so that error catching is easier
-            for (int i=0; i<clients.size();i++){
-                ClientThread tc = clients.get(i);
-                tc.sInput.close();
-                tc.sOutput.close();
-                tc.socket.close();
-            }
-        } catch (IOException e) {
-        }
-    }
+		Security.addProvider(new BouncyCastleProvider());
+		ServerSocket ss =null;
+		try
+		{
+			ss = new ServerSocket(3000);
+		}
+		catch(Exception e)
+		{
+			System.out.println("Server connection failed");
+		}
+			System.out.println("Waiting for client to connect...");
+			Socket s = ss.accept();
+			System.out.println("Client connected");
+			ObjectOutputStream dos = new ObjectOutputStream(s.getOutputStream());
+			dos.writeObject("You are connected");
+			
+			ObjectInputStream dis = new ObjectInputStream(s.getInputStream());
+			SecretKey k = (SecretKey) dis.readObject();
+			byte[] iv = (byte[])dis.readObject();
 
-    // Dispays event (not a message) to the console
-    public void display(String msg){
-        String time = sdf.format(new Date()) + " " + msg;
-        System.out.println(time);
-    }
-    // Send a message to all clients
-    private synchronized void broadcast (String message){
-        // Formats and prints message to console
-        String bcmessage = sdf.format(new Date()) + " " + message + "\n";
-        System.out.println(bcmessage);
+			//String msg = (String) dis.readObject();
+			DataInputStream in = new DataInputStream(s.getInputStream());
+			//String msg = in.readUTF();
+			byte[] message = (byte[])dis.readObject();
+			System.out.println("Received: "+Base64.getEncoder().encodeToString(message));
+			
+			System.out.println(decrypt(message, k, iv));
+			
 
-        // Sends the message to the clients
-        // In reverse order in case we need to remove someone
-        for (int i=clients.size(); --i>=0;){
-            ClientThread ct = clients.get(i);
-            // try to write to client. If a false boolean is returned then send failed,
-            // and we remove the client
-            if (!ct.writeMsg(bcmessage)){
-                clients.remove(i);
-                display(ct.username + " client disconnected");
-            }
-        }
-    }
-    private synchronized void broadcastFrom (String message,String from){
-        // Formats and prints message to console
-        String bcmessage = sdf.format(new Date()) + " " + message + "\n";
-        System.out.println(bcmessage);
-
-        // Sends the message to the clients
-        // In reverse order in case we need to remove someone
-        for (int i=0; i<clients.size();i++){
-            ClientThread ct = clients.get(i);
-            if (!(ct.username.equals(from))){
-                ct.writeMsg(bcmessage);
-            }
-        }
-    }
-
-    // used for when the client logs off using LOGOUT message
-    public synchronized void remove(int id){
-    
-        for (int i=0; i<clients.size();i++){
-            ClientThread ct = clients.get(i);
-            if (ct.id == id){
-                clients.remove(i);
-                break;
-            }
-        }
-        if(clients.isEmpty())
-        {
-          System.out.println("No more Clients");
-          try{
-          on=false;
-          serverSocket.close();
-           System.exit(0);
-          }catch(IOException e){System.out.println("Oh No");}
-         
-        }
-    }
-    public String getClientUserNames(){
-        String usernames = "";
-        for (ClientThread client : clients){
-            usernames = usernames + client.username + " ";
-        }
-        return usernames;
-    }
-    public static void main(String args[]){
-        // start server on generic port unless a port number is specified
-        int portNumber = 1500;
-        switch(args.length){
-            case 1:
-                try {
-                    portNumber = Integer.parseInt(args[0]);
-                } catch (NumberFormatException e){
-                    System.out.println("Invalid port number");
-                }
-            case 0:
-                break;
-            default:
-                System.out.println("Usage is: > java Server {portNumber}");
-                return;
-        }
-       
-        Server server=null;
-        try {
-            server = new Server(portNumber);
-        } catch (IOException e) {
-        }
-        server.start();
-
-    }
-
-    // Inner class for the thread. Easier this way than making a server object.
-    public class ClientThread extends Thread {
-
-        Socket socket;
-        ObjectInputStream sInput;
-        ObjectOutputStream sOutput;
-        int id;
-        String username;
-        Message msg;
-        String date;
-        boolean pic;
-        String message;
-        Image i;
-
-        // Thread constructor
-        public ClientThread(Socket socket){
-            id = uniqueID++;
-            this.socket = socket;
-
-            try {
-                sOutput = new ObjectOutputStream((socket.getOutputStream()));
-                sInput = new ObjectInputStream(socket.getInputStream());
-
-                // Send list of usernames to client:
-                writeMsg(getClientUserNames());
-
-                username = (String) sInput.readObject();
-
-                broadcast(username + " just connected.");//everyone should know
-
-            } catch (IOException e) {
-                display("Exception in creating data streams " + e);
-            } catch (ClassNotFoundException e) {
-            }
-            date = new Date().toString() + "\n";
-        }
-        // Loops until LOGOUT
-        @Override
-        public void run(){
-            boolean on = true;
-            while(on){
-                try {
-                    msg = (Message) sInput.readObject();
-                } catch (IOException e) {
-                    display("I/O exception. Can't read message from client.");
-                    break;
-                } catch (ClassNotFoundException e) {
-                    display("ClassNotFoundException. Can't find the object");
-                    break;
-                }
-                message = msg.getMessage();
-
-                switch (msg.getType()){
-                    case Message.MESSAGE:
-                        if(!pic){
-                        broadcastFrom(username + ": " + message,username);
-                        }
-                        
-                        break;
-                    case Message.LOGOUT:
-
-                        broadcast(username + " disconnected from server"); //i think everyone needs to know when someone leaves
-
-                        on = false;
-                        break;
-                    
-                }
-            }
-            // If here then client has logged out. So will remove client.
-            remove(id);
-            close();
-        }
-        private void close(){
-            try {
-                sOutput.close();
-                sInput.close();
-                socket.close();
-            } catch (IOException e) {
-            }
-
-        }
+	}
+	public static String decrypt (byte[] cipherText, SecretKey key,byte[] IV) throws Exception
+    {
+        //Get Cipher Instance
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
         
-        private boolean writeMsg(String msg){
-            if (!socket.isConnected()){
-                close();
-                return false;
-            }
-            // write message to the stream
-            try {
-                //System.out.println("DEBUG: Trying to write message");
-                sOutput.writeObject(msg);
-            } catch (IOException e) {
-                System.out.println("Error sending message to " + username);
-            }
-            return true;
-        }
+        //Create SecretKeySpec
+        SecretKeySpec keySpec = new SecretKeySpec(key.getEncoded(), "AES");
+        
+        //Create IvParameterSpec
+        IvParameterSpec ivSpec = new IvParameterSpec(IV);
+        
+        //Initialize Cipher for DECRYPT_MODE
+        cipher.init(Cipher.DECRYPT_MODE, keySpec, ivSpec);
+        
+        //Perform Decryption
+        byte[] decryptedText = cipher.doFinal(cipherText);
+        
+        return new String(decryptedText);
     }
-
 }
