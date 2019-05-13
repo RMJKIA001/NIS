7152cec5c4f3e5d5fedcd7044948a87dc61b47b3
PROBIE\Kiara <rmjkia001>
2019-05-13 13:21:57 +0200

    Added comments and removed extra lines

--- Client.java
+++ Client.java
@@ -3,12 +3,10 @@ import java.net.Socket;
 import java.util.Scanner;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.DataOutputStream;
 import java.security.Security;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import java.security.SecureRandom;
 import java.util.Base64;
-
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
@@ -19,49 +17,48 @@ public class Client
 {
 	public static void main(String[] args) throws Exception
 	{
+		//add bouncy castle as a service provided
 		Security.addProvider(new BouncyCastleProvider());
+		//connect to local host on port 3000
 		Socket s = new Socket("127.0.0.1",3000);
+		//input stream to recieve messages
 		ObjectInputStream dis = new ObjectInputStream(s.getInputStream());
+		//output stream to send messages
 		ObjectOutputStream dos = new ObjectOutputStream(s.getOutputStream());
 		System.out.println(dis.readObject()); //reads welcome message
+		
+		//session key generation with AES
 		KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
         keyGenerator.init(128);
-        // Generate Key
+      
         SecretKey key = keyGenerator.generateKey();
-        dos.writeObject(key);
-       	// Generating IV.
+        dos.writeObject(key); //send key to server(Needs to be encrypted)
+       	// Generate initialization vector.
         byte[] IV = new byte[16];
         SecureRandom random = new SecureRandom();
         random.nextBytes(IV);
-        dos.writeObject(IV);
+        dos.writeObject(IV); //send initialization to server (think it also needs to be encrypted?) 
         
-		DataOutputStream out =  new DataOutputStream(s.getOutputStream());
-
 		System.out.print("Enter message to send: ");
 		Scanner sc = new Scanner(System.in);
 		String msg = sc.nextLine();
-		byte[] encMsg = encrypt(msg.getBytes(), key,IV);
-		dos.writeObject(encMsg);
-		//out.writeUTF(Base64.getEncoder().encodeToString(encMsg));
+		byte[] encMsg = encrypt(msg.getBytes(), key,IV); //call to encrypt with the key an iv
+		dos.writeObject(encMsg); //sends the encryption 
 		System.out.println("Encrypted Text : "+Base64.getEncoder().encodeToString(encMsg) );
 		System.out.println("Message sent. Goodbye");
 	}
 
 	public static byte[] encrypt (byte[] plaintext,SecretKey key,byte[] IV ) throws Exception
     {
-        //Get Cipher Instance
+        //Create cipher for AES with PKCS Padding
         Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        
-        //Create SecretKeySpec
+        //Geth the spec of the genetated key, required for ciper
         SecretKeySpec keySpec = new SecretKeySpec(key.getEncoded(), "AES");
-        
-        //Create IvParameterSpec
+        //Get the spec of the generated iv, reuired for ciper
         IvParameterSpec ivSpec = new IvParameterSpec(IV);
         
-        //Initialize Cipher for ENCRYPT_MODE
+        //use the cipher to encrypt
         cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec);
-        
-        //Perform Encryption
         byte[] cipherText = cipher.doFinal(plaintext);
         
         return cipherText;
--- Server.java
+++ Server.java
@@ -1,14 +1,11 @@
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.io.DataInputStream;
-//import java.io.DataOutputStream;
 import java.io.ObjectOutputStream;
 import java.io.ObjectInputStream;
 import java.security.Security;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import java.security.SecureRandom;
 import java.util.Base64;
-
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
@@ -20,52 +17,37 @@ public class Server
 	
 	public static void main(String[] args) throws Exception
 	{
-
+		//add bouncy castle as a service provider
 		Security.addProvider(new BouncyCastleProvider());
-		ServerSocket ss =null;
-		try
-		{
-			ss = new ServerSocket(3000);
-		}
-		catch(Exception e)
-		{
-			System.out.println("Server connection failed");
-		}
-			System.out.println("Waiting for client to connect...");
-			Socket s = ss.accept();
-			System.out.println("Client connected");
-			ObjectOutputStream dos = new ObjectOutputStream(s.getOutputStream());
-			dos.writeObject("You are connected");
+		//Create a socket on port 3000
+		ServerSocket ss = new ServerSocket(3000);	
+		System.out.println("Waiting for client to connect...");
+		Socket s = ss.accept(); //Accept client connection 
+		System.out.println("Client connected");
+		ObjectOutputStream dos = new ObjectOutputStream(s.getOutputStream()); //outputstream to send message to client
+		dos.writeObject("You are connected");
 			
-			ObjectInputStream dis = new ObjectInputStream(s.getInputStream());
-			SecretKey k = (SecretKey) dis.readObject();
-			byte[] iv = (byte[])dis.readObject();
+		ObjectInputStream dis = new ObjectInputStream(s.getInputStream()); //input stream to recieve message from client
+		SecretKey k = (SecretKey) dis.readObject(); //recieves the key
+		byte[] iv = (byte[])dis.readObject(); //receives the iv
+		byte[] message = (byte[])dis.readObject(); //recieves the message
 
-			//String msg = (String) dis.readObject();
-			DataInputStream in = new DataInputStream(s.getInputStream());
-			//String msg = in.readUTF();
-			byte[] message = (byte[])dis.readObject();
-			System.out.println("Received: "+Base64.getEncoder().encodeToString(message));
-			
-			System.out.println(decrypt(message, k, iv));
+		System.out.println("Received: "+Base64.getEncoder().encodeToString(message));			
+		System.out.println(decrypt(message, k, iv)); //decrypts and prints message
 			
 
 	}
 	public static String decrypt (byte[] cipherText, SecretKey key,byte[] IV) throws Exception
     {
-        //Get Cipher Instance
+        //Create cipher for AES with PKCS Padding
         Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        
-        //Create SecretKeySpec
+        //Get the spec of the genetated key, required for ciper
         SecretKeySpec keySpec = new SecretKeySpec(key.getEncoded(), "AES");
-        
-        //Create IvParameterSpec
+        //Get the spec of the generated iv, reuired for ciper
         IvParameterSpec ivSpec = new IvParameterSpec(IV);
         
-        //Initialize Cipher for DECRYPT_MODE
+        //use cipher to decrypt
         cipher.init(Cipher.DECRYPT_MODE, keySpec, ivSpec);
-        
-        //Perform Decryption
         byte[] decryptedText = cipher.doFinal(cipherText);
         
         return new String(decryptedText);
